09ab98d06a64eb6870f2b9edeae83c2eaee9fef1
Alexander Bigerl <bigerl@mail.upb.de>
2021-02-17 18:55:05 +0100

    updated setup and LICENSE.md

--- setup.py
+++ setup.py
@@ -3,23 +3,19 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 #
-with open('LICENSE') as f:
+with open('LICENSE.md') as f:
     license = f.read()
 
 setup(
     name='sparql-query-tools',
     version='0.1.0',
-    description=readme,
-    description_content_type="text/markdown",
-    long_description=readme,
-    long_description_content_type="text/markdown",
+    description="tools to run SPARQL queries and get metrics.",
     author='Alexander Bigerl',
     author_email='info@dice-research.org',
     url='https://github.com/dice-group/sparql-query-tools',
     license="AGPL v3",
     packages=find_packages(),
-    include_package_data=True,
-classifiers=[
+    classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)", ],
     install_requires=[
@@ -27,8 +23,11 @@ classifiers=[
         "click>=7.1.2",
         "pycurl>=7.43.0.6"
     ],
+    python_requires='>=3.6',
     entry_points='''
         [console_scripts]
         sparql_result_analysis=sparql_query_tools.sparql_result_analysis.run:cli
-    '''
+    ''',
+    long_description=readme,
+    long_description_content_type="text/markdown",
 )
