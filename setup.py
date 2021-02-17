5279070d8094e16047eed72477831a6de8d2f941
Alexander Bigerl <bigerl@mail.upb.de>
2021-02-17 18:01:03 +0100

    fixed issue with LICENSE file parsing

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
-from pkg_resources import parse_requirements
 from setuptools import setup, find_packages
 
 with open('README.md') as f:
     readme = f.read()
-
+#
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
     name='sparql-query-tools',
     version='0.1.0',
-    description='Tools to get infos about SPARQL queries.',
+    description=readme,
+    description_content_type="text/markdown",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author='Alexander Bigerl',
     author_email='info@dice-research.org',
     url='https://github.com/dice-group/sparql-query-tools',
-    license=license,
+    license="AGPL v3",
     packages=find_packages(),
     include_package_data=True,
+classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)", ],
     install_requires=[
         "rdflib>=5.0.0",
         "click>=7.1.2",
