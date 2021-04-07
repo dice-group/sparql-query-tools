d6adcba59557b5da906c330043496fbd7d1cec5c
Alexander Bigerl <alexander@bigerl.eu>
2021-04-07 12:52:09 +0200

    version increment

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE.md') as f:
 
 setup(
     name='sparql-query-tools',
-    version='0.1.7',
+    version='0.1.8',
     description="tools to run SPARQL queries and get metrics.",
     author='Alexander Bigerl',
     author_email='info@dice-research.org',
