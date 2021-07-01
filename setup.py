7a02f931a2db33945ce2414f8c7eaca5c3adff9c
Alexander Bigerl <alexander@bigerl.eu>
2021-07-01 09:29:15 +0200

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE.md') as f:
 
 setup(
     name='sparql-query-tools',
-    version='0.1.8',
+    version='0.1.9',
     description="tools to run SPARQL queries and get metrics.",
     author='Alexander Bigerl',
     author_email='info@dice-research.org',
